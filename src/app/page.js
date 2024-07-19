d33fe7a0d6f4162af6a487844528ce0489f41008
Jatin Chaudhary <09jatin2005@gmail.com>
2024-07-19 19:40:49 +0530

    rotating 3d section

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import AboutUs from "@/components/AboutUs/AboutUs";
 import Hero from "@/components/Hero/Hero";
 import { Navbar } from "@/components/Navbar/Navbar";
 import OurHistory from "@/components/OurHistory/OurHistory";
-import Rotating3d from "@/components/Rotating3d/Rotating3d";
+import {Rotating3d} from "@/components/Rotating3d/Rotating3d";
 
 export default function Home() {
   return (
